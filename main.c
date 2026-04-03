edbcda281d74556bc23f469c55aec744d279d6b1
Leonel Quintero <leonelbenjaminquintero@gmail.com>
2026-04-03 18:01:42 -0300

    se modifico en rama Dev-3

--- main.c
+++ main.c
@@ -5,5 +5,17 @@ void segundo(int a, int b);
 void iguales();
 
 int main(){
-    
+
+    void primero(int a, int b){
+    printf("El primer numero (%d) es el mayor\n", a);
+}
+
+void segundo(int a, int b){
+    printf("El segundo numero (%d) es el mayor\n", a);
+}
+
+void iguales(){
+    printf("Son iguales\n");
+}
+
 }
