1d046c7cbea21ef3eb8096f1000e35c26a1bb8d8
Leonel Quintero <leonelbenjaminquintero@gmail.com>
2026-04-03 18:00:50 -0300

    se modifico en rama Dev-2

--- main.c
+++ main.c
@@ -6,4 +6,23 @@ void iguales();
 
 int main(){
     
+    int n1, n2;
+
+    printf("Ingrese el primer numero: ");
+    scanf("%d", &n1);
+    printf("Ingrese el segundo numero: ");
+    scanf("%d", &n2);
+    
+    // Dev-2
+    if (n1 > n2) {
+        primero(n1, n2);
+    } 
+    else if (n1 < n2) {
+        segundo(n1, n2);
+    } 
+    else {
+        iguales();
+    }
+    return 0;
+
 }
